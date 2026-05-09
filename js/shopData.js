fd6f2af6814b273f0c21e30a66dff7bab83c8e94
tecnoamirhan-ship-it <tecno.amirhan@gmail.com>
2026-05-09 16:13:05 +0500

    Create shopData.js

---
+++ js/shopData.js
@@ -0,0 +1,82 @@
+export const shopItems = [
+    {
+        name: 'Малое зелье здоровья',
+        icon: '🧪',
+        description: 'Восстанавливает 30 HP',
+        type: 'potion_hp',
+        effect: 30,
+        price: 15
+    },
+    {
+        name: 'Большое зелье здоровья',
+        icon: '🧪',
+        description: 'Восстанавливает 60 HP',
+        type: 'potion_hp',
+        effect: 60,
+        price: 30
+    },
+    {
+        name: 'Эликсир полного исцеления',
+        icon: '💖',
+        description: 'Восстанавливает всё HP',
+        type: 'potion_full',
+        effect: 999,
+        price: 75
+    },
+    {
+        name: 'Железный меч',
+        icon: '🗡️',
+        description: '+8 к атаке',
+        type: 'weapon',
+        effect: 8,
+        price: 50
+    },
+    {
+        name: 'Мифриловый клинок',
+        icon: '⚔️',
+        description: '+18 к атаке',
+        type: 'weapon',
+        effect: 18,
+        price: 120
+    },
+    {
+        name: 'Легендарный меч дракона',
+        icon: '🐉',
+        description: '+35 к атаке',
+        type: 'weapon',
+        effect: 35,
+        price: 300
+    },
+    {
+        name: 'Кожаный доспех',
+        icon: '🛡️',
+        description: '+5 к защите',
+        type: 'armor',
+        effect: 5,
+        price: 40
+    },
+    {
+        name: 'Стальная броня',
+        icon: '🛡️',
+        description: '+12 к защите',
+        type: 'armor',
+        effect: 12,
+        price: 100
+    },
+    {
+        name: 'Сапоги скорости',
+        icon: '👢',
+        description: '+0.8 к скорости',
+        type: 'speed',
+        effect: 0.8,
+        price: 60
+    },
+    {
+        name: 'Амулет удачи',
+        icon: '💎',
+        description: '+8% к крит.шансу',
+        type: 'crit',
+        effect: 8,
+        price: 90
+    }
+];
